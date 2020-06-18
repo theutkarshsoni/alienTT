751b05de64a7aa09cb6078d48ef021b8c6623a93
Utkarsh Soni <utkarshsoni12@gmail.com>
2020-06-18 16:30:36 +0530

    Ajax API call to get giphy

--- main.js
+++ main.js
@@ -2,8 +2,52 @@ $('#giphy-button').click(function() {
     $("#giphyModal").modal("toggle");
 });
 
+var offsetVal = 0;
+
 $('#submit-button').click(function() {
-    console.log($('#search').val());
+    offsetVal = 0;
+    $.ajax({
+        url: "http://api.giphy.com/v1/gifs/search?api_key=bBIOwAwLL2Hx7XLoIbVbBe3VtHoqjHMz&q="+ $('#search').val() +"&limit=4&offset=" + offsetVal,
+        cache: false,
+        dataType: "json",
+        type: "GET",
+        success: function(result) {
+            if(result.meta.status == 200 && result.meta.msg == "OK" && result.data.length !== 0){
+                $('#giphy-content').html("<div class='col-4 mb-3'><img src="+ result.data[0].images.fixed_height_downsampled.url +" class='img-fluid' /></div>");
+                var i = 1;
+                while(i<result.data.length){
+                    $('#giphy-content').append("<div class='col-4 mb-3'><img src="+ result.data[i].images.fixed_height_downsampled.url +" class='img-fluid' /></div>");
+                    i++;
+                }
+                if(result.pagination.total_count > result.pagination.offset){
+                    $('#more-button').css('display', 'block');    
+                    offsetVal = result.pagination.offset + 18;
+                }
+            }
+        }
+    });
+});
+
+$('#more-button').click(function() {
+    $.ajax({
+        url: "http://api.giphy.com/v1/gifs/search?api_key=bBIOwAwLL2Hx7XLoIbVbBe3VtHoqjHMz&q="+ $('#search').val() +"&limit=4&offset=" + offsetVal,
+        cache: false,
+        dataType: "json",
+        type: "GET",
+        success: function(result) {
+            if(result.meta.status == 200 && result.meta.msg == "OK" && result.data.length !== 0){
+                var i = 0;
+                while(i<result.data.length){
+                    $('#giphy-content').append("<div class='col-4 mb-3'><img src="+ result.data[i].images.fixed_height_downsampled.url +" class='img-fluid' /></div>");
+                    i++;
+                }
+                if(result.pagination.total_count > result.pagination.offset){
+                    $('#more-button').css('display', 'block');    
+                    offsetVal = result.pagination.offset + 18;
+                }
+            }
+        }
+    });
 });
 
 var quill = new Quill('#quill-editor', {
